5e20a263b27c0f6b51d8c3423f3911697c40d992
charleswang234 <wang.charles234@gmail.com>
2018-06-26 17:20:17 +0000

    created file allowing you to add to famous_people

---
+++ add_person.js
@@ -0,0 +1,32 @@
+const settings = require("./settings");
+const addFamous = process.argv.slice(2);
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+  // searchPath: ['knex', 'public'],
+});
+
+
+knex('famous_people').insert({first_name: addFamous[0], last_name: addFamous[1], birthdate: addFamous[2]})
+.asCallback(function() {
+});
+
+// knex.select('*').from('famous_people')
+// .asCallback(function(err, rows) {
+//   if (err) return console.error(err);
+//   // knex.select('id').from('nicknames')
+//   //   .whereIn('nickname', _.pluck(rows, 'name'))
+//   //   .asCallback(function(err, rows) {
+//   //     if (err) return console.error(err);
+//   //     console.log(rows);
+//   //   });
+//   console.log(rows);
+// });
